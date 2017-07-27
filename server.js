1ef40d31b3559a671363be97d244cf848c824578
Ming Tung Wong <mingtungwong@outlook.com>
2017-07-27 11:26:06 -0400

    Set up express server to send index page

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`I'm listening on port ${port}`));
+
+app.use(express.static(path.resolve(__dirname)));
+app.get('/', (req, res,next) => res.sendFile(__dirname + '/index.html'));
