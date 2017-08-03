e0801077096c4941f22363c5fdf5b65c10a3c68f
Ming Tung Wong <mingtungwong@outlook.com>
2017-08-02 23:22:20 -0400

    Fix minor typo

--- components/SiteRule.jsx
+++ components/SiteRule.jsx
@@ -13,10 +13,10 @@ class SiteRule extends React.Component {
         const ruleType = rules.rule;
 
         const ruleString = ruleStringMap[ruleType];
-        let quantityString = "";
+        let quantityString = " ";
         if(rules.rule === "Range") quantityString = ` ${rules.quantity[0]} and ${rules.quantity[1]} `;
         else if(rules.rule === "Minimum") quantityString = ` ${rules.quantity[0]} `;
-        else if(rules.rule === "Maximum") quantityString = `${rules.quantity[1]}`;
+        else if(rules.rule === "Maximum") quantityString = ` ${rules.quantity[1]}`;
         
         const categoryString = `${rules.category}`;
 
