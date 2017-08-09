f4c0d69a7f0c2d228724a5cd827a9d30fd78819c
Ming Tung Wong <mingtungwong@outlook.com>
2017-08-09 12:04:55 -0400

    Added intro text to home and some styling changes

--- components/home.jsx
+++ components/home.jsx
@@ -4,7 +4,14 @@ class Home extends React.Component {
     render() {
         return (
             <div className="home">
-                Hello
+                <h3>Welcome to Password Rules</h3>
+                <p className="textBlock">
+                    Password Rules is a website where users can submit password requirements for websites that they use.
+                    Oftentimes password requirements only differ slightly among websites. One may require a special character,
+                    while another may forbid it. When that occurs, it may not be worth the hassle to have the entire password
+                    reset for a one time login rather than seeing the rules and possibly remembering that password. Constantly
+                    resetting the password only prolongs the problem rather than fixing it.
+                </p>
             </div>
         )
     }
