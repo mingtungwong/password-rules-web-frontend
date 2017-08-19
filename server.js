a1e1b329e1beea32f5a8406d423c9f3bef133df2
Ming Tung Wong <mingtungwong@outlook.com>
2017-08-18 23:18:29 -0400

    Minor changes

--- server.js
+++ server.js
@@ -6,4 +6,4 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`I'm listening on port ${port}`));
 
 app.use(express.static('public'));
-app.get('/*', (req, res,next) => res.sendFile(__dirname + '/index.html'));
+app.get('/*', (req, res,next) => res.sendFile(path.resolve(__dirname + '/index.html')));
